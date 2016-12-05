b389f6103007ac2d7e189435bf24e17512fc1bed
Emmanuel VANHOUCKE <emmanuel.vanhoucke@a-sis.com>
2016-12-05 14:16:30 +0100

    get env mysql var

--- config/config_db.inc.php
+++ config/config_db.inc.php
@@ -1,7 +1,7 @@
 <?php
-define( 'DB_TYPE', 'mysql' );
-define( 'DB_USER', 'testlink' );
-define( 'DB_PASS', 'testlink' );
-define( 'DB_HOST', 'localhost' );
-define( 'DB_NAME', 'testlink' );
-define( 'DB_TABLE_PREFIX', '' );
+define('DB_TYPE', if(isset($_SERVER['DB_TYPE']) ? $_SERVER['DB_TYPE']: 'mysql');
+define('DB_USER', if(isset($_SERVER['DB_USER']) ? $_SERVER['DB_USER']: 'root');
+define('DB_PASS', if(isset($_SERVER['DB_PASS']) ? $_SERVER['DB_PASS']: 'root');
+define('DB_HOST', if(isset($_SERVER['DB_HOST']) ? $_SERVER['DB_HOST']: 'database');
+define('DB_NAME', if(isset($_SERVER['DB_NAME']) ? $_SERVER['DB_NAME']: 'testlink');
+?>
